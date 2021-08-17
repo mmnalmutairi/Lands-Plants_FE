a007d207c46eddddf9746dfb21c4ce371f818a43
Maha Almutairi <carsridder10@gmail.com>
2021-08-17 13:59:46 +0300

    Maha's Update

--- components/plants/Explore.js
+++ components/plants/Explore.js
@@ -1,5 +1,13 @@
-import React from "react";
-import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { useNavigation } from "@react-navigation/core";
@@ -7,6 +15,44 @@ import authStore from "../stores/authStore";
 
 const Explore = () => {
   const navigation = useNavigation();
+  const [plants, setPlants] = useState([
+    {
+      id: 0,
+      name: "Apples",
+      image: require("../../assets/Apples.jpeg"),
+      categoryId: 1,
+    },
+    {
+      id: 1,
+      name: "Banana's",
+      image: require("../../assets/Banana.jpeg"),
+      categoryId: 1,
+    },
+    {
+      id: 2,
+      name: "Cherries",
+      image: require("../../assets/Cherries.jpeg"),
+      categoryId: 1,
+    },
+    {
+      id: 3,
+      name: "Coriander",
+      image: require("../../assets/Coriander.jpeg"),
+      categoryId: 2,
+    },
+    {
+      id: 4,
+      name: "Oranges",
+      image: require("../../assets/Oranges.jpeg"),
+      categoryId: 1,
+    },
+    {
+      id: 5,
+      name: "Watermelon",
+      image: require("../../assets/Watermelon.jpeg"),
+      categoryId: 1,
+    },
+  ]);
   const FilteringPlants = () => {
     // Should filter the Plants according to the categories
     // const filteredplant = plantStore.plants.filter(
@@ -21,23 +67,97 @@ const Explore = () => {
     navigation.navigate("Home");
   };
 
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        onPress={handlePress}
+  const renderPlants = (item, index) => {
+    return (
+      <View
         style={{
-          marginLeft: 390,
-          paddingTop: 60,
-          paddingBottom: 10,
+          alignItems: "center",
+          justifyContent: "center",
+          marginHorizontal: 10,
+          marginVertical: -20,
         }}
       >
-        <AntDesign name="logout" size={24} color="white" />
-      </TouchableOpacity>
-      {/* <View style={styles.header}> */}
-      <View style={{ height: "30%", backgroundColor: "white" }}>
-        {/* Plants List according to categories */}
+        <Image
+          source={item.image}
+          resizeMode="cover"
+          style={{
+            width: width * 0.31,
+            height: "70%",
+            backgroundColor: "white",
+            borderRadius: 20,
+          }}
+        />
+        <View
+          style={{
+            position: "absolute",
+            bottom: "19%",
+            right: 0,
+            backgroundColor: "#00996D",
+            paddingHorizontal: 12,
+            borderTopLeftRadius: 10,
+            borderBottomLeftRadius: 10,
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 18, fontWeight: "bold" }}>
+            {item.name}
+          </Text>
+        </View>
       </View>
-      <View style={{ height: "45%", backgroundColor: "lightgray" }}>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* <View style={styles.header}> */}
+      <View style={{ height: "40%", backgroundColor: "white" }}>
+        {/* Plants List according to categories */}
+        <View
+          style={{
+            flex: 1,
+            borderBottomLeftRadius: 50,
+            borderBottomRightRadius: 50,
+            backgroundColor: "#00996D",
+            marginTop: -100,
+          }}
+        >
+          <TouchableOpacity
+            onPress={handlePress}
+            style={{
+              marginLeft: 390,
+              paddingTop: 160,
+              paddingBottom: 10,
+            }}
+          >
+            <AntDesign name="logout" size={24} color="white" />
+          </TouchableOpacity>
+          <View style={{ marginTop: -29, marginHorizontal: 24 }}>
+            <View>
+              <Text
+                style={{ color: "white", fontSize: 22, fontWeight: "bold" }}
+              >
+                Plants
+              </Text>
+              <TouchableOpacity
+                onPress={() => {
+                  console.log("Focus on filteration");
+                }}
+              >
+                <Image />
+              </TouchableOpacity>
+            </View>
+            <View style={{ marginTop: 8 }}>
+              <FlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                data={plants}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item, index }) => renderPlants(item, index)}
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={{ height: "45%", backgroundColor: "#edf2f4" }}>
         {/* What to plant these days */}
       </View>
       {/* </View> */}
@@ -117,15 +237,11 @@ const Explore = () => {
   );
 };
 
+const { width, height } = Dimensions.get("screen");
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#43aa8b",
-  },
-  header: {
-    flex: 2,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#edf2f4",
   },
   footer: {
     flex: 0.02,
